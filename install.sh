b408b016b7589504f557df581368404ad1c970a2
Marco <marco.isfc@gmail.com>
2026-08-21 14:11:42 +0200

    update install script

--- install.sh
+++ install.sh
@@ -2,8 +2,19 @@ setup-dns
 apk update
 apk -U upgrade
 reboot
-apk add curl docker git
+apk add curl docker docker-cli-compose git
 rc-update add docker default
 rc-update add docker boot
 service docker start
-ssh-keygen
+git clone https://github.com/SiestaCat/vps-proxy.git
+cd vps-proxy
+cp .env.tmpl .env
+# edit .env
+docker compose up -d --build
+
+# How to test it:
+
+curl \
+  --proxy "https://proxy.example.com:443" \
+  --proxy-user "proxyuser:change-me" \
+  https://ifconfig.me
